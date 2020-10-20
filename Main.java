9e29cf3618ac711dc7dd82670e4ca6f5dcf89048
Jmalone20 <jasmine.malone@patelhs.org>
2020-10-20 12:21:38 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int myAge = 14 
+    int birthdate = 4/29/06
+    int today'sDate = 10/20/20
+  
+    System.out.println(" I am "+ myAge + " years old ");
+
+    if
+
+  }
+}
