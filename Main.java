0762a71ae305b275500b91027c9916240c5ca346
Jmalone20 <jasmine.malone@patelhs.org>
2020-10-21 13:14:12 +0000

    I added and if/Else statement

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 class Main {
   public static void main(String[] args) {
-    int myAge = 14 
-    int birthdate = 4/29/06
-    int today'sDate = 10/20/20
-  
-    System.out.println(" I am "+ myAge + " years old ");
-
-    if
-
+    int myAge = 14;
+    int birthyear = 2006;
+    int todaysYear = 2020;
+     {
+      if(todaysYear - birthyear = myAge){
+      System.out.println(" I am " + myAge + " years old ");
+        }
+        else{
+         System.out.println(" I am still " + myAge + "years old "); 
+      }
+     }
   }
 }
